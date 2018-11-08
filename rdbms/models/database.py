e9dffbfc42cfe0b7454c6717cd648e7e3752059c
Ethan Sternke <ethan.sternke@gmail.com>
2018-11-08 11:21:45 -0500

    environment variable

--- rdbms/models/database.py
+++ rdbms/models/database.py
@@ -1,8 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from dotenv import load_dotenv
+import os
 
-engine = create_engine('sqlite:///development.db', echo=True)
+load_dotenv('./.env')
+
+engine = create_engine(os.environ['DATABASE'], echo=True)
 connection = engine.connect()
 Session = sessionmaker(bind=connection)
 session = Session()
